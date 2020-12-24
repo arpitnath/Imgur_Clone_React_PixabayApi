160cb164fb29f9f4df528fc292e84e99a775015f
arpitnath <arpitnath42@gmail.com>
2020-12-25 02:28:18 +0530

    setting up the pixabay apikey & fetched data sucessfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}+flowers&image_type=photo`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-      <h1>test</h1>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://images.unsplash.com/photo-1608097172838-f8094d967aa4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 }
